110dc7126b9732a82411b16c351aacbff29f5945
HelloJacobW <jawoa4d@student.oaisd.org>
2024-10-16 18:35:53 +0000

    Finally figured out what I needed

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       string boo = "******";
+       Console.WriteLine(boo);
+       boo = boo.Substring(0,boo.Length-1);
+       Console.WriteLine(boo);
+    }
+}
